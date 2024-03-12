c309e6827c0d4f5a2f2012ac4105d539afd56cb2
TotallyRod <rtorregrosalopez@outlook.es>
2024-03-12 20:03:45 +0100

    Jump Pad maybe?

---
+++ code/JumpPadScript.cs
@@ -0,0 +1,37 @@
+using Sandbox;
+
+public sealed class JumpPadScript : Component, Component.ITriggerListener
+{
+	[Property] private float force { get; set; }
+	[Property] private float delayT { get; set; }
+	private List<Rigidbody> delayed;
+	protected override void OnAwake()
+	{
+		delayed = new List<Rigidbody>();
+	}
+	async void delay( Rigidbody delayedBody )
+	{
+		delayed.Add( delayedBody );
+		await Task.DelaySeconds( delayT );
+		if ( delayedBody != null ) delayed.Remove( delayedBody );
+	}
+
+	void ITriggerListener.OnTriggerEnter( Collider other )
+	{
+		Rigidbody rb = other.Components.Get<Rigidbody>();
+		if ( rb != null )
+		{
+			if ( !delayed.Contains( rb ) )
+			{
+				rb.ApplyForce( Transform.World.Up * force );
+				delay( rb );
+			}
+		}
+
+	}
+
+	void ITriggerListener.OnTriggerExit( Collider other )
+	{
+
+	}
+}
