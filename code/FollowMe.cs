1d2a558e4d6c1625ab037ae36896953743301abe
TotallyRod <rtorregrosalopez@outlook.es>
2024-03-13 19:58:37 +0100

    Shadow fix, other stuff

--- code/FollowMe.cs
+++ code/FollowMe.cs
@@ -3,9 +3,11 @@ using Sandbox;
 public sealed class FollowMe : Component
 {
 	[Property] GameObject target { get; set; }
+	[Property] private float Offset { get; set; }
 
 	protected override void OnFixedUpdate()
 	{
-		Transform.Position = new Vector3( target.Transform.Position.x, target.Transform.Position.y, Transform.Position.z );
+		Vector3 desPos = new Vector3( target.Transform.Position.x, target.Transform.Position.y, target.Transform.Position.z + Offset );
+		Transform.Position = desPos;
 	}
 }
