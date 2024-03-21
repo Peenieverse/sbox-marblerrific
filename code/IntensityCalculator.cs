11b4d97eaed88598f16b4b61ee94e932dedb35a6
thehumbleonion <160753124+SwagAccount@users.noreply.github.com>
2024-03-22 00:37:18 +0700

    woops

--- code/IntensityCalculator.cs
+++ code/IntensityCalculator.cs
@@ -12,17 +12,19 @@ public sealed class IntensityCalculator : Component
 		{
 			if(go.Tags.Has("player"))
 			{
-				playerBody = go.Components.Get<Rigidbody>();
+				Rigidbody rb = go.Components.Get<Rigidbody>();
+				if(rb!=null) playerBody = rb;
 			}
 		}
 	}
 	protected override void OnUpdate()
 	{
 		//TROLLFACEINREALLIFE: structured like this for future additions to intensity calc
+		Log.Info((playerBody.Velocity.x+playerBody.Velocity.y+playerBody.Velocity.z));
 		float Intensity = MathX.Clamp(
-		(SpeedIntenseMult*(playerBody.Velocity.x+playerBody.Velocity.y+playerBody.Velocity.z))
+		SpeedIntenseMult*(playerBody.Velocity.x+playerBody.Velocity.y+playerBody.Velocity.z)
 		,0,1);
 
-		MusicPlayer.Intensity = MathX.FloorToInt(Intensity*MusicPlayer.MTransitionsSPC.Count)-1;
+		MusicPlayer.Intensity = MathX.FloorToInt(MathX.Clamp(Intensity*MusicPlayer.MTransitionsSPC.Count,1,500))-1;
 	}
 }
