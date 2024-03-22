914191de1be735f6dc613931cd7a37a4aa98c4e6
thehumbleonion <160753124+SwagAccount@users.noreply.github.com>
2024-03-22 16:10:02 +0700

    Improved Dynamic Music

--- code/DynamicMusicPlayer.cs
+++ code/DynamicMusicPlayer.cs
@@ -5,12 +5,13 @@ using Sandbox.UI;
 public sealed class DynamicMusicPlayer : Component
 {
 	[Property] public List<SoundPointComponent> MTransitionsSPC {get;set;}
+	[Property] private float Volume {get; set;}
 	[Property] public int Intensity {get;set;}
 	protected override void OnUpdate()
 	{
 		for(int i = 0; i < MTransitionsSPC.Count; i++)
 		{
-			MTransitionsSPC[i].Volume = MathX.Lerp(MTransitionsSPC[i].Volume, i == Intensity ? 1 : 0, Time.Delta);
+			MTransitionsSPC[i].Volume = MathX.Lerp(MTransitionsSPC[i].Volume, i == Intensity ? Volume : 0, Time.Delta);
 		}
 	}
 }
--- code/IntensityCalculator.cs
+++ code/IntensityCalculator.cs
@@ -5,9 +5,12 @@ public sealed class IntensityCalculator : Component
 	Rigidbody playerBody;
 	[Property] private DynamicMusicPlayer MusicPlayer {get;set;}
 	[Property] private float SpeedIntense {get; set;}
-	[Property] private float SpeedIntenseMult {get; set;}
+	[Property] private float SpeedLevel {get; set;}
 	[Property] private float SpeedIntenseAdd {get; set;}
 	[Property] private float SpeedIntenseRemove {get; set;}
+
+	//this could be soem sort of level rating thing
+	[Property] private float FinalIntensityLevel {get; set;}
 	protected override void OnStart()
 	{
 		IEnumerable<GameObject> SceneObjects = Scene.GetAllObjects(true);
@@ -20,18 +23,27 @@ public sealed class IntensityCalculator : Component
 			}
 		}
 	}
+	float finalIntensity;
 	protected override void OnUpdate()
 	{
 		//TROLLFACEINREALLIFE: structured like this for future additions to intensity calc
 		Vector3 velAbs = playerBody.Velocity.Abs();
 		float speed = (velAbs.x+velAbs.y+velAbs.z);
-		//TROLLFACEINREALLIFE: have to divide by 100 because inspector cant go so low
-		SpeedIntense = MathX.Clamp(SpeedIntense+(SpeedIntenseAdd/100)*speed*Time.Delta,0,1);
-		SpeedIntense = MathX.Clamp(SpeedIntense-SpeedIntenseRemove*Time.Delta,0,1);
+		if(speed > SpeedLevel)
+		{
+			SpeedIntense += Time.Delta*SpeedIntenseAdd * speed/100;
+		}
+		else
+		{
+			SpeedIntense -= Time.Delta*SpeedIntenseRemove;
+		}
+		SpeedIntense = SpeedIntense.Clamp(0,1);
 		float Intensity = MathX.Clamp(
 		SpeedIntense
 		,0,1);
 
 		MusicPlayer.Intensity = (int)Math.Round((double)MathX.Clamp(Intensity*MusicPlayer.MTransitionsSPC.Count,1,500))-1;
+		finalIntensity += Intensity*Time.Delta;
+		FinalIntensityLevel = finalIntensity/Time.Now;
 	}
 }
